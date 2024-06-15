c554a118640cb4d3829c9164a6dfcd3e083f9d79
tunnelWithAC <conalldalydev@gmail.com>
2024-06-15 11:21:59 +0100

    tidy code

--- V2/transform.py
+++ V2/transform.py
@@ -1,4 +1,4 @@
-from collections.abc import Iterable
+from itertools import groupby
 
 
 class Transform:
@@ -10,19 +10,14 @@ class Transform:
         return other
 
     def __call__(self, input):
-        # this may need to be revised in order to use groupby
-        """
-        currently we process per input
-
-        for groups we want to process the iterator
-        """
         groupbys = []
         transforms = []
-        for c in self.children:
-            if isinstance(c, GroupBy):
-                groupbys.append(c)
+        # inputs are processed differently based on regular transform (per input) vs groupby
+        for child in self.children:
+            if isinstance(child, GroupBy):
+                groupbys.append(child)
             else:
-                transforms.append(c)
+                transforms.append(child)
 
         transform_outputs = map(self.expand, input)
 
@@ -61,15 +56,9 @@ class Transform:
     def expand(self, input_or_inputs):
         raise NotImplementedError
 
-    # def iterate_children(self, transform_output, children):
-    #     pass
-
 
 class GroupBy(Transform):
-    # def __init__(self, get_key_fn):
-    #     self.get_key_fn = get_key_fn
     def __init__(self):
-        # super.__init__()
         super().__init__()
 
     def iterate_children(self, transform_output, children):
@@ -77,17 +66,6 @@ class GroupBy(Transform):
         return parent.iterate_children(transform_output, children)
 
     def __call__(self, input):
-        """
-        input: (x, 4), (y, 14), (z, 3), (x, 4),
-        expected output: (x, 7), (y, 14), (z, 3)
-        function: sum_iterables
-        """
-        from itertools import groupby
-
-        # if not isinstance(input, list):
-        #     input = list(input)
-
-
         groups = {}
         for key, group in groupby(input, self.get_key()):
             if key not in groups.keys():
@@ -97,12 +75,6 @@ class GroupBy(Transform):
 
         yield from self.iterate_children(groups, self.children)
 
-    def get_key(self):
-
+    @staticmethod
+    def get_key():
         return lambda x: x[0]
-
-#
-# class Temp(Transform):
-#     def __init__(self):
-#         s = super()
-#         super().__init__()
